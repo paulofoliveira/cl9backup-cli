4b399d416e7d4feed2d3568dc800315b2372966a
Paulo Fernando <paulo.fdo@hotmail.com>
2023-02-14 21:55:22 -0300

    Lógica inicial de processo do Login

--- src/Cl9Backup.CLI/Program.cs
+++ src/Cl9Backup.CLI/Program.cs
@@ -21,11 +21,43 @@ public class Program
     }
 
     [Command(Name = "cl9backup", Description = "Executa rotinas de backup e restore seguindo configurações em sua área gerenciado em https://backup.cl9.cloud/")]
-    [HelpOption]
+    [HelpOption(Description = "Mostra informações de ajuda")]
     public class CliCommands
     {
+        [Option("-l | --login", CommandOptionType.NoValue, Description = "Guarda credenciais para execução do processo de Login nas chamadas da API", ShowInHelpText = true)]
+        public bool Login { get; set; }
         public Task<int> OnExecute(CommandLineApplication app, IConsole console)
         {
+            if (Login)
+            {
+                var login = string.Empty;
+                var password = string.Empty;
+
+                console.WriteLine($"***** Credenciais do CL9 Backup *****");
+
+                while (string.IsNullOrEmpty(login))
+                {
+                    login = Prompt.GetString("Login:");
+
+                    if (string.IsNullOrEmpty(login))
+                        console.WriteLine($"O campo Login é obrigatório.");
+                }
+
+                while (string.IsNullOrEmpty(password))
+                {
+                    password = Prompt.GetPassword("Senha:");
+
+                    if (string.IsNullOrEmpty(password))
+                        console.WriteLine($"O campo Senha é obrigatório.");
+                }
+
+                console.WriteLine($"Armazenando credenciais...");
+
+                // TODO: Persiste na Local Storage.
+
+                return Task.FromResult(OK);
+            }
+
             return Task.FromResult(OK);
         }
     }
