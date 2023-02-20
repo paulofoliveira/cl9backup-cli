0a9733776b48861996cf2ebe049c9a21ab79cf87
Paulo Fernando <paulo.fdo@hotmail.com>
2023-02-20 20:10:59 -0300

    Repositório base para LiteDb implementado

---
+++ src/Cl9Backup.CLI/Domain/Persistence/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Cl9Backup.CLI.Domain.Persistence
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Update(T entity);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Delete(int id);
+    }
+}
---
+++ src/Cl9Backup.CLI/Infrastructure/Persistence/Repository.cs
@@ -0,0 +1,30 @@
+﻿using Cl9Backup.CLI.Domain.Persistence;
+using LiteDB;
+
+namespace Cl9Backup.CLI.Infrastructure.Persistence
+{
+    internal class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly LiteDatabase _db;
+        private readonly string _collectionName;
+        public Repository(LiteDatabase db, string collectionName)
+        {
+            _db = db;
+            _collectionName = collectionName;
+        }
+        public void Add(T entity) => Collection.Insert(entity);
+        public void Delete(int id) => Collection.Delete(new BsonValue(id));
+        public IEnumerable<T> GetAll() => Collection.FindAll();
+        public T GetById(int id)
+        {
+            if (id <= 0)
+                return default!;
+
+            var item = Collection.FindById(new BsonValue(id));
+
+            return item;
+        }
+        public void Update(T entity) => Collection.Update(entity);
+        protected ILiteCollection<T> Collection => _db.GetCollection<T>(_collectionName);
+    }
+}
