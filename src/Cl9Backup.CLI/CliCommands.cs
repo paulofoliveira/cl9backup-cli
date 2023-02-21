5c9c923178a665b72ca4b51de4cd577bb4de4fbb
Paulo Fernando <paulo.fdo@hotmail.com>
2023-02-21 18:23:38 -0300

    Leitura dos parâmetros do comando de Backup

--- src/Cl9Backup.CLI/CliCommands.cs
+++ src/Cl9Backup.CLI/CliCommands.cs
@@ -20,6 +20,18 @@ namespace Cl9Backup.CLI
         [Option("-c|--config", CommandOptionType.NoValue, Description = "Realiza as configurações necessárias para execuções de Backup/Restore. Serve também como \"reset\" caso precise atualizar as configuraçoes", ShowInHelpText = true)]
         public bool Config { get; set; }
 
+        [Option("-b|--backup", CommandOptionType.NoValue, Description = "Executa operação de Backup conforme as configurações de destino, fonte e dispositivo", ShowInHelpText = true)]
+        public bool Backup { get; set; }
+
+        [Option("-d|--destination", CommandOptionType.SingleValue, Description = "Nome do Destino (Bucket)", ShowInHelpText = true)]
+        public string? Destination { get; set; }
+
+        [Option("-s|--source", CommandOptionType.SingleValue, Description = "Nome da Fonte do Backup", ShowInHelpText = true)]
+        public string? Source { get; set; }
+
+        [Option("-dv|--device", CommandOptionType.SingleValue, Description = "Nome do dispositivo conectado", ShowInHelpText = true)]
+        public string? Device { get; set; }
+
         public Task<int> OnExecute(CommandLineApplication app, IConsole console)
         {
             var isConfigured = _parametroRepository.IsConfigured();
@@ -73,7 +85,40 @@ namespace Cl9Backup.CLI
                 return Task.FromResult(Constants.OK);
             }
 
+            if (Backup)
+            {
+                console.WriteTitle("Execução de Backup do CL9 Backup");
+
+                while (string.IsNullOrEmpty(Destination))
+                {
+                    Destination = Prompt.GetString("Destino:");
+                    if (string.IsNullOrEmpty(Destination))
+                        console.WriteLine($"O campo Destino é obrigatório.");
+                }
+
+                while (string.IsNullOrEmpty(Source))
+                {
+                    Source = Prompt.GetString("Fonte:");
+                    if (string.IsNullOrEmpty(Source))
+                        console.WriteLine($"O campo Fonte é obrigatório.");
+                }
+
+                while (string.IsNullOrEmpty(Device))
+                {
+                    Device = Prompt.GetString("Dispositivo:");
+                    if (string.IsNullOrEmpty(Device))
+                        console.WriteLine($"O campo Dispositivo é obrigatório.");
+                }
+
+                // TODO: Autenticar na API para obtenção da SessionKey.
+                // TODO: Chamar endpoint GetProfileAndHash para recuperar valores de Destination, Source e Device.
+
+                console.WriteLine($"Executando backup para {Destination} da fonte {Source} através do dispositivo {Device}...");
+            }
+
             return Task.FromResult(Constants.OK);
         }
     }
 }
+
+
